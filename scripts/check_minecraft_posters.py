f9c411cbc3c71b3e1b45b5e833d78a666d8e20b9
syoubu <oregagaiki0311@gmail.com>
2026-09-23 03:41:23 +0900

    Bump poster catalog pack versions for client refresh

--- scripts/check_minecraft_posters.py
+++ scripts/check_minecraft_posters.py
@@ -43,7 +43,7 @@ class PosterChecks(unittest.TestCase):
         }
 
     def test_versions(self):
-        for pack, version in ((BP, [1, 0, 31]), (RP, [1, 0, 36]), (IMPORT, [1, 0, 29])):
+        for pack, version in ((BP, [1, 0, 32]), (RP, [1, 0, 37]), (IMPORT, [1, 0, 29])):
             with self.subTest(pack=pack.name):
                 manifest = read_json(pack / "manifest.json")
                 self.assertEqual(manifest["header"]["version"], version)
